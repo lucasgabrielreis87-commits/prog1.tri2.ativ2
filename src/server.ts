457cb01eec2e4543c671aa615e181042ee0a2c98
Lucas Gabriel <lucasgabrielreis87@gmail.com>
2026-06-25 21:38:24 -0300

    editar itens no server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,98 @@
-import TodoList, { Item } from './core'
-const todolist = new TodoList('todolist.json')
+import TodoListClass, { Item } from "./core"
+const todolist = new TodoListClass("todolist.json")
 
-async function requetTest(req: Bun.BunRequest) {
-  return Response.json({
-    method: req.method,
-    time: new Date().toLocaleString('pt-BR'),
-    body: await req.body?.text(),
-  });
+async function testRoute(req: Bun.BunRequest) {
+    return Response.json({
+        method: req.method,
+        time: new Date().toLocaleString('pt-BR'),
+        body: await req.body?.text(),
+        key: crypto.randomUUID()
+    });
 }
 
 const server = Bun.serve({
-  port: 3000,
-  routes: {
-    '/api-debugger': (req) => new Response(Bun.file('./public/api-debugger.html')),
-    '/test': {
-      GET: requetTest,
-      POST: requetTest,
-      PUT: requetTest,
-      DELETE: requetTest,
-      PATCH: requetTest,
-      OPTIONS: requetTest,
-    },
-    '/todo': {
-        GET: async () => {
-            const items = await todolist.getItems()
-            return Response.json(items)
+    port: 3000,
+    routes: {
+        '/': (req) => new Response(Bun.file('./public/index.html')),
+        '/api-debugger': (req) => new Response(Bun.file('./public/api-debugger.html')),
+        '/test': testRoute,
+        '/todo': {
+            GET: async () => {
+                const items = await todolist.getItems()
+                return Response.json(items)
+            },
+
+            POST: async (req) => {
+                let data
+
+                try {
+                    data = await req.body?.json()
+                } catch (e) {
+                    return new Response('json inválido', { status: 400 })
+                }
+
+                if (!data?.title)
+                    return new Response('É preciso informar title', { status: 400 })
+
+                let index: number
+
+                try {
+                    index = await todolist.addItem(new Item(data.title))
+                } catch (error) {
+                    return new Response('Erro ao adicionar item', { status: 500 })
+                }
+
+                return Response.json({ index }, { status: 201 })
+            }
         },
-        POST: async (req) => {
-            let data
+        '/todo/:index': {
+            GET: (req) => {
+                return new Response("bhruu", { status: 501 })
+            },
+            DELETE: async (req) => {
+                const strIndex = req.params.index
+                const index = parseInt(strIndex)
+                if (isNaN(index))
+                    return new Response('/todo/:index index precisa ser um número', { status: 400 })
+                try {
 
-            try {
-                data = await req.body?.json()
-            } catch (e) {
-                return new Response('json inválido', { status: 400 })
+                    await todolist.removeItem(index)
+
+                    return new Response(`Item do index ${index} removido.`)
+                } catch (e) {
+                    return new Response(`Item do index ${index} não existe.`, { status: 400 })
+                }
+            },
+            PUT: async (req) => {
+                let data
+                const indexStr = req.params.index;
+                const index = parseInt(indexStr);
+
+                if (isNaN(index)) {
+                    return new Response('Erro, precisa de um número', { status: 400 })
+                }
+
+                try {
+                    data = await req.body?.json();
+                } catch (e) {
+                    return new Response('Json do corpo mal formatado', { status: 400 })
+                }
+                if (!data?.title) {
+                    return new Response('É preciso informar title', { status: 400 })
+                }
+
+                try {
+                    await todolist.updateItem(index, new Item(data.title));
+                } catch (e) {
+                    return new Response('Erro ao atualizar ITEM:', { status: 500 })
+                }
+                return new Response("Atualizado com sucesso");
             }
-
-            if (!data?.title)
-                return new Response('É preciso informar title', { status: 400 })
-
-            try {
-                await todolist.addItem(new Item(data.title))
-            } catch (error) {
-                return new Response('Erro ao adicionar item', { status: 500 })
-            }
-
-            return new Response('Created', { status: 201 })
         }
     },
-    '/todo/:index': async (req) => {
-        const indexStr = req.params.index
-        const index = parseInt(indexStr)
-        if (isNaN(index)) 
-            return new Response('index precisa ser um número inteiro', { status: 400 })
-        await todolist.removeItem(index)
-        return new Response(`Item de indice ${index}, removido com sucesso`)
-    } 
-  },
-  fetch(req) {
-    return new Response("Not Found", { status: 404 });
-  },
+    fetch(req) {
+        return new Response("Not Found", { status: 404 });
+    },
 });
 
 console.log(`Server running at http://localhost:${server.port}`);
